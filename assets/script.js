9c2837a506df570db8ce265de1ae59c57251ea3d
mohseenHamid <mhsn@hotmail.co.uk>
2023-01-21 19:17:48 +0000

    refine getPassword & generatePassword functions

--- assets/script.js
+++ assets/script.js
@@ -95,6 +95,12 @@ let optUppercase;
 let optNumeric;
 let optSpecialChar;
 
+// Function for getting a random element's value from an array
+function getRandom(arr) {
+	let arrIndex = Math.floor(Math.random() * arr.length);
+	return arr[arrIndex];
+}
+
 // Shuffle array (inspired by stack)
 // src: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
@@ -111,26 +117,6 @@ function shuffle(array) {
 	}
 	return array;
 }
-// ORIGINAL CODE FROM STACK
-// function shuffle(array) {
-//   // Setting the index of the variable being shifted to the final element
-// 	let currentIndex = array.length;
-// 	let randomIndex = "";
-
-// 	// While there remain elements to shuffle.
-// 	while (currentIndex != 0) {
-// 		// Pick a remaining element.
-// 		randomIndex = Math.floor(Math.random() * currentIndex);
-// 		currentIndex--;
-
-// 		// And swap it with the current element.
-// 		[array[currentIndex], array[randomIndex]] = [
-// 			array[randomIndex],
-// 			array[currentIndex]
-// 		];
-// 	}
-// 	return array;
-// }
 
 // Function to prompt user for password options
 function getPasswordOptions() {
@@ -153,27 +139,8 @@ function getPasswordOptions() {
 		);
 		getPasswordOptions();
 	} else {
-		// If input is valid, alert the user to the requirement of selecting at least one of the character categories to construct the password from
-		alert(
-			`Please confirm at least one of the following four categories to construct the password from \n\n1. Lowercase (a-z) \n2. Uppercase (A-Z) \n3. Numeric (0-9) \n4. Special (e.g. $ @ % & *)`
-		);
 		// Requesting the user to confirm character category preferences
-		optLowercase = confirm(
-			`1. Would you like lowercase characters (a-z) in your password?`
-		);
-		optUppercase = confirm(
-			`2. Would you like uppercase characters (A-Z) in your password?`
-		);
-		optNumeric = confirm(
-			`3. Would you like numeric characters (0-9) in your password?`
-		);
-		optSpecialChar = confirm(
-			`4. Would you like special characters ($ @ % & *) in your password?`
-		);
-
-		// Resetting the category selection in the instance all 4 categories are rejected
-		while (!optLowercase && !optUppercase && !optNumeric && !optSpecialChar) {
-			alert(`You must confirm at least one category!`);
+		const chooseCategories = function () {
 			optLowercase = confirm(
 				`1. Would you like lowercase characters (a-z) in your password?`
 			);
@@ -186,45 +153,51 @@ function getPasswordOptions() {
 			optSpecialChar = confirm(
 				`4. Would you like special characters ($ @ % & *) in your password?`
 			);
+		};
+
+		// If input is valid, alert the user to the requirement of selecting at least one of the character categories to construct the password from
+		alert(
+			`Please confirm at least one of the following four categories to construct the password from \n\n1. Lowercase (a-z) \n2. Uppercase (A-Z) \n3. Numeric (0-9) \n4. Special (e.g. $ @ % & *)`
+		);
+		chooseCategories();
+
+		// Resetting the category selection in the instance all 4 categories are rejected
+		while (!optLowercase && !optUppercase && !optNumeric && !optSpecialChar) {
+			alert(`You must confirm at least one category!`);
+			chooseCategories();
 		}
 	}
 }
 
-// Function for getting a random element's value from an array
-function getRandom(arr) {
-	let arrIndex = Math.floor(Math.random() * arr.length);
-	return arr[arrIndex];
-}
-
 // Function to generate password with user input
 function generatePassword() {
-	// Define empty arrays
+	// Define arrays
 	let secretWord = [];
 	let masterCategoryArray = [];
-	let charCategoriesArray = [];
+	let charCategoriesChoiceArray = [
+		optLowercase,
+		optUppercase,
+		optNumeric,
+		optSpecialChar
+	];
+	let charCategoriesArray = [
+		lowerCasedCharacters,
+		upperCasedCharacters,
+		numericCharacters,
+		specialCharacters
+	];
+	let count = 0;
 
-	if (optLowercase) {
-		charCategoriesArray.push("x");
-		// Adding the selected category array to a masterCategoryArray to determine the remaining characters (remainChar) from
-		masterCategoryArray = masterCategoryArray.concat(lowerCasedCharacters);
-	}
-	if (optUppercase) {
-		charCategoriesArray.push("x");
-		masterCategoryArray = masterCategoryArray.concat(upperCasedCharacters);
-	}
-	if (optNumeric) {
-		charCategoriesArray.push("x");
-		masterCategoryArray = masterCategoryArray.concat(numericCharacters);
-	}
-	if (optSpecialChar) {
-		charCategoriesArray.push("x");
-		masterCategoryArray = masterCategoryArray.concat(specialCharacters);
+	for (let i = 0; i < charCategoriesArray.length; i++) {
+		if (charCategoriesChoiceArray[i]) {
+			masterCategoryArray = masterCategoryArray.concat(charCategoriesArray[i]);
+			count++;
+		}
 	}
 
 	// Simple maths to determine how many categories were selected + how many characters to take from each category + how many remaining characters are needed
-	let numCharCategories = charCategoriesArray.length;
-	let numCharPerCategory = Math.floor((numChar * 1) / numCharCategories);
-	let remainChar = numChar - numCharPerCategory * numCharCategories;
+	let numCharPerCategory = Math.floor((numChar * 1) / count);
+	let remainChar = numChar - numCharPerCategory * count;
 
 	// Generating the password according to the maths above + selected categories
 	if (optLowercase) {
